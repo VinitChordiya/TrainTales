8824cc27b9d360d6d695254431a04a2fbecdbb29
Pritha Dilip Kawli <Dilip@Kawli.Pritha>
2024-06-11 22:57:26 +0530

    work_div update + others

--- python_docs/ds_docs/data_prep.py
+++ python_docs/ds_docs/data_prep.py
@@ -4,45 +4,45 @@ from nltk.tokenize import sent_tokenize
 
 
 def load_data(file_path):
-    """
-    Loads data from a file.
+    # """
+    # Loads data from a file.
 
-    Args:
-        file_path (str): The path to the file.
+    # Args:
+    #     file_path (str): The path to the file.
 
-    Returns:
-        str: The content of the file.
+    # Returns:
+    #     str: The content of the file.
 
-    Raises:
-        FileNotFoundError: If the file does not exist.
-        IOError: If there is an error reading the file.
-    """
+    # Raises:
+    #     FileNotFoundError: If the file does not exist.
+    #     IOError: If there is an error reading the file.
+    # """
     with open(file_path, "r") as file:
         return file.read()
 def preprocess_data(complaints):
-    """
-    Preprocesses the given complaints by tokenizing them into sentences and creating a DataFrame with sentiment data.
+    # """
+    # Preprocesses the given complaints by tokenizing them into sentences and creating a DataFrame with sentiment data.
 
-    Args:
-        complaints (str): The complaints to be preprocessed.
+    # Args:
+    #     complaints (str): The complaints to be preprocessed.
 
-    Returns:
-        pandas.DataFrame: A DataFrame containing sentiment data for each sentence in the complaints.
-    """
+    # Returns:
+    #     pandas.DataFrame: A DataFrame containing sentiment data for each sentence in the complaints.
+    # """
     sentences = sent_tokenize(complaints)
     
     sentiment_data = [{'sentence': sentence} for sentence in sentences]
     return pd.DataFrame(sentiment_data)
 def add_dummy_timestamps(sentiment_df):
-    """
-    Adds dummy timestamps to a DataFrame.
+    # """
+    # Adds dummy timestamps to a DataFrame.
 
-    Parameters:
-    sentiment_df (pandas.DataFrame): The DataFrame to add timestamps to.
+    # Parameters:
+    # sentiment_df (pandas.DataFrame): The DataFrame to add timestamps to.
 
-    Returns:
-    pandas.DataFrame: The DataFrame with dummy timestamps added.
-    """
+    # Returns:
+    # pandas.DataFrame: The DataFrame with dummy timestamps added.
+    # """
     start_date = datetime.now()
     timestamps = [start_date - timedelta(days=i) for i in range(len(sentiment_df))]
     sentiment_df['timestamp'] = timestamps
